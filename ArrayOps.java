8a4d045a955b5b6921546feb71868a0a8a633e50
ElizaKnapp <eknapp20@stuy.edu>
2020-10-23 12:09:19 -0400

    Sum is completed

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,3 +1,10 @@
 public class ArrayOps {
   //here is where the methods will be
+  public static int sum(int[] arr) {
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
 }
