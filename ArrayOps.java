00aca21909b04c775805d80d1bb38b2f370946e1
ElizaKnapp <eknapp20@stuy.edu>
2020-10-23 13:27:25 -0400

    Added sumRows and also the array to string converter from the last homework to help with the test cases

--- ArrayOps.java
+++ ArrayOps.java
@@ -17,5 +17,22 @@ public class ArrayOps {
     }
     return largest;
   }
-  
+
+  public static int[] sumRows(int[][] matrix) {
+    int[] ans = new int[matrix.length];
+    for (int i = 0; i < matrix.length; i++) {
+      ans[i] = sum(matrix[i]);
+    }
+    return ans;
+  }
+
+  public static String arrToString(int[] arr) {
+    if (arr.length == 0) return "{}";
+    String ans = "{";
+    for (int i = 0; i < arr.length - 1;i++) {
+      ans += Integer.toString(arr[i]) + ", ";
+    }
+    return ans + Integer.toString(arr[arr.length - 1]) + "}";
+  }
+
 }
