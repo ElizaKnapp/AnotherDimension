be65fc07c27e656580f13c65fe26d7a27e1d6745
ElizaKnapp <eknapp20@stuy.edu>
2020-10-24 14:29:42 -0400

    Now sumCols is functional

--- ArrayOps.java
+++ ArrayOps.java
@@ -46,7 +46,7 @@ public class ArrayOps {
     int[][] swapped = new int[matrix[0].length][matrix.length];
     for (int i = 0; i < matrix.length; i++) {
       for (int j = 0; j < matrix[0].length; j++) {
-        matrix[i][j] = swapped[j][i];
+        swapped[j][i] = matrix[i][j];
       }
     }
     return sumRows(swapped);
