28741a184575aebf3d851724ca9e8000452acb73
ElizaKnapp <eknapp20@stuy.edu>
2020-10-24 14:55:09 -0400

    Test cases have comments with expected values

--- Tester.java
+++ Tester.java
@@ -5,16 +5,16 @@ public class Tester {
   public static void main(String[] args) {
     //Where all the test cases and print statements are
     int[] test1 = {1, 3, 4};
-    System.out.println(ArrayOps.sum(test1));
-    System.out.println(ArrayOps.largest(test1));
+    System.out.println(ArrayOps.sum(test1)); //8
+    System.out.println(ArrayOps.largest(test1)); //4
 
     int[][] test2 = { {3, 2, 5}, {2, 1, 6}, {6, 7, 8}};
-    System.out.println(Arrays.toString(ArrayOps.sumRows(test2)));
-    System.out.println(Arrays.toString(ArrayOps.largestInRows(test2)));
-    System.out.println(ArrayOps.sum(test2));
+    System.out.println(Arrays.toString(ArrayOps.sumRows(test2))); //10, 9, 21
+    System.out.println(Arrays.toString(ArrayOps.largestInRows(test2))); //5, 6, 8
+    System.out.println(ArrayOps.sum(test2)); //40
 
     //Part 5
-    System.out.println(Arrays.toString(ArrayOps.sumCols(test2)));
+    System.out.println(Arrays.toString(ArrayOps.sumCols(test2))); //11, 10, 19
 
     int[][] test3 = { {  1,  2, 3, 4 },{  2, 3,  4,  1 }, { 3, 4,  1, 2 } };
     System.out.println(ArrayOps.isRowMagic(test3)); //true
@@ -30,7 +30,7 @@ public class Tester {
     System.out.println(ArrayOps.isColMagic(test5));
 
     int[][] E = { { 2,  4, 2 }, {  2, 2, 2 } };
-    System.out.println(ArrayOps.isLocationMagic(E, 0, 1 //false
+    System.out.println(ArrayOps.isLocationMagic(E, 0, 1)); //false
     System.out.println(ArrayOps.isLocationMagic(E, 1, 1)); //true
   }
 }
