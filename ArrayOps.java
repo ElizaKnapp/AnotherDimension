dff48996807cffc924e88f565ce00157fc90e657
ElizaKnapp <eknapp20@stuy.edu>
2020-10-24 14:26:27 -0400

    Functional double array sum and sumCols is fine but not yet tested

--- ArrayOps.java
+++ ArrayOps.java
@@ -34,4 +34,22 @@ public class ArrayOps {
     return ans;
   }
 
+  public static int sum(int[][] arr) {
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      sum += sum(arr[i]);
+    }
+    return sum;
+  }
+
+  public static int[] sumCols(int[][] matrix) {
+    int[][] swapped = new int[matrix[0].length][matrix.length];
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        matrix[i][j] = swapped[j][i];
+      }
+    }
+    return sumRows(swapped);
+  }
+
 }
